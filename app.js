e8046e26a9db31a7c1bf10d880b0df310b5ec7e7
lou-flan <louise_flanagan@outlook.com>
2020-07-07 18:14:34 +1000

    Created bookings controller and routes with function shells in each to complete

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const bookingRouter = require('./routes/bookings_routes');
 
 const port = process.env.PORT || 3030;
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+	res.send('Welcome to Tooth Inc!');
+});
+
+app.use('/bookings', bookingRouter);
 
 app.listen(port, () => {
 	console.log(`Tooth Inc Server running on port ${port}`);
