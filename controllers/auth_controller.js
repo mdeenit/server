1486fbb4a0a9bc6c9f0d5b832560531cc7aab3e0
lou-flan <louise_flanagan@outlook.com>
2020-07-10 07:40:54 +1000

    Auth register working and tested in postman

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,7 +1,29 @@
 const passport = require('passport');
 const User = require('../models/user');
 
-const register = (req, res) => {};
+const register = (req, res) => {
+	User.register(
+		new User({
+			username: req.body.username,
+			email: req.body.email,
+		}),
+		req.body.password,
+		(error) => {
+			if (error) {
+				res.status(500);
+				res.json({
+					error: error,
+				});
+			} else {
+				passport.authenticate('local')(req, res, () => {
+					console.log('authenticated user: ', req.user.username);
+					console.log('session: ', req.session);
+					res.json(req.user);
+				});
+			}
+		}
+	);
+};
 
 const login = (req, res) => {};
 
--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { register, login, logout } = require('../controllers/auth_controller');
 
-router.post('register', register);
+router.post('/register', register);
 
 module.exports = router;
