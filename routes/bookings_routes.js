9be259c5b1930fa0ed74544393cbe9ca81eaf8c0
lou-flan <louise_flanagan@outlook.com>
2020-07-15 17:24:34 +1000

    Moved user authenticated back to cover all booking routes. Added cors config into App.js

--- routes/bookings_routes.js
+++ routes/bookings_routes.js
@@ -11,13 +11,13 @@ const {
 } = require('../controllers/bookings_controller');
 
 // Authenticate user for all routes
-
+router.use(userAuthenticated);
 // Read all bookings
 router.get('/', getBookings);
 
 // Read booking with :id
 router.get('/:id', getBooking);
-router.use(userAuthenticated); // THIS SHOULD BE ON LINE 14
+
 // Make a booking
 router.post('/', makeBooking);
 
